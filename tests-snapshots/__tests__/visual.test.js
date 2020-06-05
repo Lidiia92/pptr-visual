7460570d0d02d1bcba0737b33ed2a6568efdc9a1
Lidii <lidiiany92@gmail.com>
2020-06-05 11:50:07 -0700

    added tablet and mobile tests

--- tests-snapshots/__tests__/visual.test.js
+++ tests-snapshots/__tests__/visual.test.js
@@ -43,4 +43,36 @@ describe('Visual Regression Testing', () => {
 			failureTreshold: 0.01,
 		});
 	});
+
+	test('Mobile Snapshot', async function () {
+		await page.goto('https://www.example.com');
+		await page.waitForSelector('h1');
+		await page.emulate(puppeteer.devices['iPhone X']);
+		const image = await page.screenshot();
+		expect(image).toMatchImageSnapshot({
+			failureTresholdType: 'percent',
+			failureTreshold: 0.01,
+		});
+	});
+
+	test('Tablet Snapshot', async function () {
+		await page.goto('https://www.example.com');
+		await page.waitForSelector('h1');
+		await page.emulate(puppeteer.devices['iPad landscape']);
+		const image = await page.screenshot();
+		expect(image).toMatchImageSnapshot({
+			failureTresholdType: 'percent',
+			failureTreshold: 0.01,
+		});
+	});
+
+	test('Remove Element Before Snapshot', async function () {
+		await page.goto('https://www.example.com');
+		await page.evaluate(() => {
+			(document.querySelectorAll('h1') || []).forEach((el) =>
+				el.remove()
+			);
+		});
+		await page.waitFor(5000);
+	});
 });
